babc51a60f57ae7b83bbc38781e989cd77be73d9
noureddein <noureddein@domedia.me>
2025-01-14 00:34:36 +0300

    Fixed tanstack router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { UseAPIs } from "@hooks/useAPIs";
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import * as React from "react";
 
 type RouterContext = {
@@ -12,6 +12,18 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 	component: RootComponent,
 	notFoundComponent: () => <p>404, Not found page.</p>,
 });
+
+const TanStackRouterDevtools =
+	process.env.VITE_NODE_ENV === "production"
+		? () => null // Render nothing in production
+		: React.lazy(() =>
+				// Lazy load in development
+				import("@tanstack/router-devtools").then((res) => ({
+					default: res.TanStackRouterDevtools,
+					// For Embedded Mode
+					// default: res.TanStackRouterDevtoolsPanel
+				}))
+			);
 
 function RootComponent() {
 	return (
