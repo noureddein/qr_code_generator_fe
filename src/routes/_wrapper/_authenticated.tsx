4f03500203a02ac9d884abda512a64418d9dfb05
noureddein <noureddein@domedia.me>
2025-01-14 00:51:11 +0300

    Ading not found page

--- src/routes/_wrapper/_authenticated.tsx
+++ src/routes/_wrapper/_authenticated.tsx
@@ -1,14 +1,17 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_wrapper/_authenticated')({
-  beforeLoad: async ({ context: { apis } }) => {
-    console.log('Loading 2')
-    const response = await apis.identity()
-    if (!response.isLoggedIn) {
-      throw redirect({ to: '/login' })
-    }
-  },
-  pendingComponent: () => {
-    return <div>Loading...</div>
-  },
-})
+export const Route = createFileRoute("/_wrapper/_authenticated")({
+	beforeLoad: async ({ context: { apis } }) => {
+		console.log("Loading 2");
+		const response = await apis.identity();
+		if (!response.isLoggedIn) {
+			throw redirect({ to: "/login" });
+		}
+	},
+	pendingComponent: () => {
+		return <div>Loading...</div>;
+	},
+	notFoundComponent: () => {
+		return <p>Desired page not found</p>;
+	},
+});
